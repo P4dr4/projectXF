59760f1b9a1c10cd4d0ad4b6b1ebef0382ad9573
Pedro Accarini Barros de Azevedo <122038856+P4dr4@users.noreply.github.com>
2024-11-05 17:12:41 -0300

    Feature / CI (#1)

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { HomeComponent } from './home.component';
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: MockRouter;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [],
+      declarations: [HomeComponent],
+      providers: [{ provide: Router, useClass: MockRouter }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router) as any;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to info page', () => {
+    component.goToInfo();
+    expect(router.navigate).toHaveBeenCalledWith(['/info']);
+  });
+
+  it('should navigate to login page', () => {
+    component.goToLogin();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should navigate to home page', () => {
+    component.goToHome();
+    expect(router.navigate).toHaveBeenCalledWith(['']);
   });
 });
--- src/app/info/info.component.spec.ts
+++ src/app/info/info.component.spec.ts
@@ -1,23 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { InfoComponent } from './info.component';
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('InfoComponent', () => {
   let component: InfoComponent;
   let fixture: ComponentFixture<InfoComponent>;
+  let router: MockRouter;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InfoComponent]
-    })
-    .compileComponents();
+      imports: [InfoComponent],
+      providers: [{ provide: Router, useClass: MockRouter }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InfoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router) as any;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to info page', () => {
+    component.goToInfo();
+    expect(router.navigate).toHaveBeenCalledWith(['/info']);
+  });
+
+  it('should navigate to login page', () => {
+    component.goToLogin();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should navigate to home page', () => {
+    component.goToHome();
+    expect(router.navigate).toHaveBeenCalledWith(['']);
   });
 });
