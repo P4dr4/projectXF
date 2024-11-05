c6957ede199d18698a2504e120cd4058abd90ad9
P4dr4 <paccarini.bar@outlook.com>
2024-11-04 22:38:23 -0300

    Adjusting CI unity tests

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // deixa a saída do Jasmine visível no navegador
+      clearContext: false,
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/project-xf'),
@@ -24,7 +24,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: true, // Define o Karma para rodar uma vez e finalizar
+    singleRun: true,
     restartOnFileChange: true
   });
 };
